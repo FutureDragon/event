89a4f550bed10a4fded7452a9950b86582240bde
futuredragon <future_dragon@hotmail.de>
2016-02-15 11:52:04 +0100

    Add Typ Field to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215100947) do
+ActiveRecord::Schema.define(version: 20160215105053) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160215100947) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "typ"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
