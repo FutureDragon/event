332ad4cff823659d4f11af884a8afa900a0362ff
futuredragon <future_dragon@hotmail.de>
2016-02-16 22:51:21 +0100

    Verschickte Anfragen werden im Profil angezeigt

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @anfrage = Anfrage.where(empfaenger_id: current_user.id)
+    @anfrageBekommen = Anfrage.where(empfaenger_id: current_user.id)
+    @anfrageGeschrieben = Anfrage.where(absender_id: current_user.id)
   end
 end
